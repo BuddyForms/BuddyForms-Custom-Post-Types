043827fc1bb97810d937d09df8137866999c6127
Sven Lehnert <mail@sven-lehnert.de>
2016-11-17 11:15:37 +0100

    add conditional logic

--- loader.php
+++ loader.php
@@ -34,117 +34,115 @@ add_action( 'init', 'buddyforms_custom_post_types_init', 20 );
 function buddyforms_custom_post_types_init() {
 	global $buddyforms;
 
-
 	foreach ( $buddyforms as $form_slug => $buddyform ){
 
-		if ( isset( $buddyform['name'] ) ) {
-			$labels['name'] = $buddyform['name'];
-		}
-		if ( isset( $buddyform['singular_name'] ) ) {
-			$labels['singular_name'] = $buddyform['singular_name'];
-		}
-		if ( isset( $buddyform['labels']['menu_name'] ) ) {
-			$labels['menu_name'] = $buddyform['labels']['menu_name'];
-		}
-		if ( isset( $buddyform['labels']['all_items'] ) ) {
-			$labels['all_items'] = $buddyform['labels']['all_items'];
-		}
-		if ( isset( $buddyform['labels']['add_new'] ) ) {
-			$labels['add_new'] = $buddyform['labels']['add_new'];
-		}
-		if ( isset( $buddyform['labels']['add_new_item'] ) ) {
-			$labels['add_new_item'] = $buddyform['labels']['add_new_item'];
-		}
-		if ( isset( $buddyform['labels']['edit_item'] ) ) {
-			$labels['edit_item'] = $buddyform['labels']['edit_item'];
-		}
-		if ( isset( $buddyform['labels']['new_item'] ) ) {
-			$labels['new_item'] = $buddyform['labels']['new_item'];
-		}
-		if ( isset( $buddyform['labels']['view_item'] ) ) {
-			$labels['view_item'] = $buddyform['labels']['view_item'];
-		}
-		if ( isset( $buddyform['labels']['search_items'] ) ) {
-			$labels['search_items'] = $buddyform['labels']['search_items'];
-		}
-		if ( isset( $buddyform['labels']['not_found'] ) ) {
-			$labels['not_found'] = $buddyform['labels']['not_found'];
-		}
-		if ( isset( $buddyform['labels']['not_found_in_trash'] ) ) {
-			$labels['not_found_in_trash'] = $buddyform['labels']['not_found_in_trash'];
-		}
-		if ( isset( $buddyform['labels']['parent_item_colon'] ) ) {
-			$labels['parent_item_colon'] = $buddyform['labels']['parent_item_colon'];
-		}
-		if ( isset( $buddyform['labels']['featured_image'] ) ) {
-			$labels['featured_image'] = $buddyform['labels']['featured_image'];
-		}
-		if ( isset( $buddyform['labels']['set_featured_image'] ) ) {
-			$labels['set_featured_image'] = $buddyform['labels']['set_featured_image'];
-		}
-		if ( isset( $buddyform['labels']['remove_featured_image'] ) ) {
-			$labels['remove_featured_image'] = $buddyform['labels']['remove_featured_image'];
-		}
-		if ( isset( $buddyform['labels']['use_featured_image'] ) ) {
-			$labels['use_featured_image'] = $buddyform['labels']['use_featured_image'];
-		}
-		if ( isset( $buddyform['labels']['archives'] ) ) {
-			$labels['archives'] = $buddyform['labels']['archives'];
-		}
-		if ( isset( $buddyform['labels']['insert_into_item'] ) ) {
-			$labels['insert_into_item'] = $buddyform['labels']['insert_into_item'];
-		}
-		if ( isset( $buddyform['labels']['uploaded_to_this_item'] ) ) {
-			$labels['uploaded_to_this_item'] = $buddyform['labels']['uploaded_to_this_item'];
-		}
-		if ( isset( $buddyform['labels']['filter_items_list'] ) ) {
-			$labels['filter_items_list'] = $buddyform['labels']['filter_items_list'];
-		}
-		if ( isset( $buddyform['labels']['items_list_navigation'] ) ) {
-			$labels['items_list_navigation'] = $buddyform['labels']['items_list_navigation'];
-		}
-		if ( isset( $buddyform['labels']['items_list'] ) ) {
-			$labels['items_list'] = $buddyform['labels']['items_list'];
-		}
-		if ( isset( $buddyform['labels']['parent_item_colon'] ) ) {
-			$labels['parent_item_colon'] = $buddyform['labels']['parent_item_colon'];
-		}
+		if($buddyform['post_type'] == 'custom'){
 
-		$args = array(
-			'label'               => $labels['name'],
-			'labels'              => $labels,
-			'description'         => $buddyform['description'],
-			'public'              => isset($buddyform['public']) ? true : false,
-			'publicly_queryable'  => isset($buddyform['publicly_queryable']) ? true : false,
-			'show_ui'             => isset($buddyform['show_ui']) ? true : false,
-			'show_in_rest'        => isset($buddyform['show_in_rest']) ? true : false,
-			'rest_base'           => isset($buddyform['rest_base']) ? $buddyform['public'] : '',
-			'has_archive'         => isset($buddyform['has_archive']) ? true : false,
-			'show_in_menu'        => isset($buddyform['show_in_menu']) ? true : false,
-			'exclude_from_search' => isset($buddyform['exclude_from_search']) ? true : false,
-			'capability_type'     => isset($buddyform['capability_type']) ? $buddyform['public'] : 'post',
-			'map_meta_cap'        => isset($buddyform['map_meta_cap']) ? true : false,
-			'hierarchical'        => isset($buddyform['hierarchical']) ? true : false,
-			'rewrite'             => array(
-				'slug' => $buddyform['slug'],
-				'with_front' => isset($buddyform['with_front']) ? true : false,
-			),
-			'query_var'           => isset($buddyform['query_var']) ? true : false,
-			'supports'            => isset($buddyform['supports']) ?  array(
-				'title',
-				'editor',
-				'thumbnail'
-			) : array(),
-			'taxonomies'          => isset($buddyform['taxonomies']) ?  array(
-				'product_tag',
-			) : array(),
-		);
-		register_post_type( $buddyform['slug'] , $args );
+			if ( isset( $buddyform['name'] ) ) {
+				$labels['name'] = $buddyform['name'];
+			}
+			if ( isset( $buddyform['singular_name'] ) ) {
+				$labels['singular_name'] = $buddyform['singular_name'];
+			}
+			if ( isset( $buddyform['labels']['menu_name'] ) ) {
+				$labels['menu_name'] = $buddyform['labels']['menu_name'];
+			}
+			if ( isset( $buddyform['labels']['all_items'] ) ) {
+				$labels['all_items'] = $buddyform['labels']['all_items'];
+			}
+			if ( isset( $buddyform['labels']['add_new'] ) ) {
+				$labels['add_new'] = $buddyform['labels']['add_new'];
+			}
+			if ( isset( $buddyform['labels']['add_new_item'] ) ) {
+				$labels['add_new_item'] = $buddyform['labels']['add_new_item'];
+			}
+			if ( isset( $buddyform['labels']['edit_item'] ) ) {
+				$labels['edit_item'] = $buddyform['labels']['edit_item'];
+			}
+			if ( isset( $buddyform['labels']['new_item'] ) ) {
+				$labels['new_item'] = $buddyform['labels']['new_item'];
+			}
+			if ( isset( $buddyform['labels']['view_item'] ) ) {
+				$labels['view_item'] = $buddyform['labels']['view_item'];
+			}
+			if ( isset( $buddyform['labels']['search_items'] ) ) {
+				$labels['search_items'] = $buddyform['labels']['search_items'];
+			}
+			if ( isset( $buddyform['labels']['not_found'] ) ) {
+				$labels['not_found'] = $buddyform['labels']['not_found'];
+			}
+			if ( isset( $buddyform['labels']['not_found_in_trash'] ) ) {
+				$labels['not_found_in_trash'] = $buddyform['labels']['not_found_in_trash'];
+			}
+			if ( isset( $buddyform['labels']['parent_item_colon'] ) ) {
+				$labels['parent_item_colon'] = $buddyform['labels']['parent_item_colon'];
+			}
+			if ( isset( $buddyform['labels']['featured_image'] ) ) {
+				$labels['featured_image'] = $buddyform['labels']['featured_image'];
+			}
+			if ( isset( $buddyform['labels']['set_featured_image'] ) ) {
+				$labels['set_featured_image'] = $buddyform['labels']['set_featured_image'];
+			}
+			if ( isset( $buddyform['labels']['remove_featured_image'] ) ) {
+				$labels['remove_featured_image'] = $buddyform['labels']['remove_featured_image'];
+			}
+			if ( isset( $buddyform['labels']['use_featured_image'] ) ) {
+				$labels['use_featured_image'] = $buddyform['labels']['use_featured_image'];
+			}
+			if ( isset( $buddyform['labels']['archives'] ) ) {
+				$labels['archives'] = $buddyform['labels']['archives'];
+			}
+			if ( isset( $buddyform['labels']['insert_into_item'] ) ) {
+				$labels['insert_into_item'] = $buddyform['labels']['insert_into_item'];
+			}
+			if ( isset( $buddyform['labels']['uploaded_to_this_item'] ) ) {
+				$labels['uploaded_to_this_item'] = $buddyform['labels']['uploaded_to_this_item'];
+			}
+			if ( isset( $buddyform['labels']['filter_items_list'] ) ) {
+				$labels['filter_items_list'] = $buddyform['labels']['filter_items_list'];
+			}
+			if ( isset( $buddyform['labels']['items_list_navigation'] ) ) {
+				$labels['items_list_navigation'] = $buddyform['labels']['items_list_navigation'];
+			}
+			if ( isset( $buddyform['labels']['items_list'] ) ) {
+				$labels['items_list'] = $buddyform['labels']['items_list'];
+			}
+			if ( isset( $buddyform['labels']['parent_item_colon'] ) ) {
+				$labels['parent_item_colon'] = $buddyform['labels']['parent_item_colon'];
+			}
+
+			$args = array(
+				'label'               => $labels['name'],
+				'labels'              => $labels,
+				'description'         => $buddyform['description'],
+				'public'              => isset($buddyform['public']) ? true : false,
+				'publicly_queryable'  => isset($buddyform['publicly_queryable']) ? true : false,
+				'show_ui'             => isset($buddyform['show_ui']) ? true : false,
+				'show_in_rest'        => isset($buddyform['show_in_rest']) ? true : false,
+				'rest_base'           => isset($buddyform['rest_base']) ? $buddyform['public'] : '',
+				'has_archive'         => isset($buddyform['has_archive']) ? true : false,
+				'show_in_menu'        => isset($buddyform['show_in_menu']) ? true : false,
+				'exclude_from_search' => isset($buddyform['exclude_from_search']) ? true : false,
+				'capability_type'     => isset($buddyform['capability_type']) ? $buddyform['public'] : 'post',
+				'map_meta_cap'        => isset($buddyform['map_meta_cap']) ? true : false,
+				'hierarchical'        => isset($buddyform['hierarchical']) ? true : false,
+				'rewrite'             => array(
+					'slug' => $buddyform['slug'],
+					'with_front' => isset($buddyform['with_front']) ? true : false,
+				),
+				'query_var'           => isset($buddyform['query_var']) ? true : false,
+				'supports'            => isset($buddyform['supports']) ?  array(
+					'title',
+					'editor',
+					'thumbnail'
+				) : array(),
+				'taxonomies'          => isset($buddyform['taxonomies']) ?  array(
+					'product_tag',
+				) : array(),
+			);
+			register_post_type( $buddyform['slug'] , $args );
+		}
 	}
-
-
-
-// End of cptui_register_my_cpts_bformalslug()
 }
 
 function buddyforms_form_setup_nav_li_cpt() { ?>
@@ -160,6 +158,8 @@ function buddyforms_form_setup_tab_pane_cpt() {
 	global $post, $buddyform; ?>
 	<div class="tab-pane fade in" id="cpt">
 	<div class="buddyforms_accordion_cpt">
+		<h4><?php _e('Custom Post Type'); ?></h4>
+		<ul>
 
 		<?php
 
@@ -170,8 +170,6 @@ function buddyforms_form_setup_tab_pane_cpt() {
 		if ( ! $buddyform ) {
 		$buddyform = get_post_meta( get_the_ID(), '_buddyforms_options', true );
 		}
-
-		echo '<h4>' . __( 'Custom Post Type Setup', 'buddyforms' ) . '</h4><br>';
 
 		$form_setup[]  = new Element_Checkbox( '<b>' . __( 'Public', 'buddyforms' ) . '</b>', "buddyforms_options[public]", array( 'public' => __( 'public', 'buddyforms' ) ), array(
 			'value'     => isset( $buddyform['public'] ) ? $buddyform['public'] : '',
@@ -261,11 +259,8 @@ function buddyforms_form_setup_tab_pane_cpt() {
 		) );
 
 
-		buddyforms_display_field_group_table( $form_setup );
+		buddyforms_formbuilder_settings( 'cpt_setup', 'Setup', $form_setup );
 
-
-
-		echo '<h4>' . __( 'Custom Post Type Label', 'buddyforms' ) . '</h4><br>';
 
 		$form_setup = array();
 		$form_setup[]  = new Element_Textbox( '<b>' . __( 'Menu Name', 'buddyforms' ) . '</b>', "buddyforms_options[labels][menu_name]", array(
@@ -356,12 +351,89 @@ function buddyforms_form_setup_tab_pane_cpt() {
 			'value'     => isset( $buddyform['labels']['parent_item_colon'] ) ? $buddyform['labels']['parent_item_colon'] : '',
 			'shortDesc' => 'Description'
 		) );
-		buddyforms_display_field_group_table( $form_setup );
+		buddyforms_formbuilder_settings( 'cpt_label', 'Labels', $form_setup );
 
 
 		?>
+		</ul>
 	</div>
 	</div><?php
 }
 
 add_action( 'buddyforms_form_setup_tab_pane_last', 'buddyforms_form_setup_tab_pane_cpt' );
+
+
+
+function buddyforms_formbuilder_settings( $trigger, $link_label, $form_setup ){
+	?>
+	<li id="trigger<?php echo $trigger ?>" class="bf_trigger_list_item <?php echo $trigger ?> ui-sortable-handle">
+		<div class="accordion_fields">
+			<div class="accordion-group">
+				<div class="accordion-heading-options">
+					<table class="wp-list-table widefat fixed posts">
+						<tbody>
+						<tr>
+							<td class="field_label">
+								<a style="display: block;" class="bf_edit_field row-title accordion-toggle collapsed" data-toggle="collapse"
+								   data-parent="#accordion_text" href="#accordion_<?php echo $trigger ?>"
+								   title="Edit this Field"
+								   href="#">
+								<strong>
+									<?php echo $link_label; ?>
+								</strong>
+								</a>
+							</td>
+						</tr>
+						</tbody>
+					</table>
+				</div>
+				<div id="accordion_<?php echo $trigger ?>" class="accordion-body collapse">
+					<div class="accordion-inner">
+						<?php buddyforms_display_field_group_table( $form_setup, $trigger ) ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</li>
+<?php
+
+}
+
+add_filter('buddyforms_form_builder_post_type', 'buddyforms_cpt_form_builder_post_type');
+function buddyforms_cpt_form_builder_post_type($post_types){
+	$post_types['custom'] = 'Custom';
+	return $post_types;
+}
+
+
+add_action( 'buddyforms_admin_js_footer', 'buddyforms_cpt_admin_js_footer' );
+function buddyforms_cpt_admin_js_footer(){
+
+	?>
+	<script>
+		jQuery(document).ready(function (jQuery) {
+
+			// Init function to check the post type and show the custom post type nav is needed
+			from_setup_post_type_custom();
+
+			// On Change listener for the post type. If the post type is custom show the custom post type nav
+			jQuery(document.body).on('change', '#form_post_type', function () {
+				from_setup_post_type_custom();
+			});
+
+		});
+
+		// function to show hide the custom post type nav
+		function from_setup_post_type_custom() {
+			var post_type = jQuery('#form_post_type').val();
+			if(post_type == 'custom'){
+				jQuery('.cpt-nav').show();
+			} else {
+				jQuery('.cpt-nav').hide();
+			}
+		}
+
+	</script>
+	<?php
+
+}
